d4f8ba696cd0444aba1694bae45c663f73baeebf
tarang00 <taranglilhare@gmail.com>
2025-01-24 02:45:42 +0530

    Add all my files

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// Select all 'Join Now' buttons
-document.querySelectorAll('.price__btn').forEach(button => {
-    button.addEventListener('click', (event) => {
-      // Get the plan details
-      const planCard = event.target.closest('.price__card');
-      const planName = planCard.querySelector('h4').innerText;
-      const planPrice = planCard.querySelector('h3').innerText;
+// // Select all 'Join Now' buttons
+// document.querySelectorAll('.price__btn').forEach(button => {
+//     button.addEventListener('click', (event) => {
+//       // Get the plan details
+//       const planCard = event.target.closest('.price__card');
+//       const planName = planCard.querySelector('h4').innerText;
+//       const planPrice = planCard.querySelector('h3').innerText;
   
-      // Redirect to a payment page with query parameters
-      const paymentUrl = `payment.html?plan=${encodeURIComponent(planName)}&price=${encodeURIComponent(planPrice)}`;
-      window.location.href = paymentUrl;
-    });
-  });
+//       // Redirect to a payment page with query parameters
+//       const paymentUrl = `payment.html?plan=${encodeURIComponent(planName)}&price=${encodeURIComponent(planPrice)}`;
+//       window.location.href = paymentUrl;
+//     });
+//   });
   
